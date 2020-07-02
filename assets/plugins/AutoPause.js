928990dd6b823671807d1fc2b35badec9ccd9962
Willy Da Conceicao <willydavidlozada@gmail.com>
2020-07-02 10:18:57 -0500

    VisibilityChange

--- assets/plugins/AutoPause.js
+++ assets/plugins/AutoPause.js
@@ -2,6 +2,7 @@ class AutoPause {
     constructor() {
         this.threshold = 0.25
         this.handlerIntersection = this.handlerIntersection.bind(this)
+        this.handlerVisibilityChange = this.handlerVisibilityChange.bind(this)
     }
     run(player) {
         this.player = player
@@ -10,6 +11,8 @@ class AutoPause {
             threshold: this.threshold, //define el % de intersección
         })
         observer.observe(this.player.media)
+
+        document.addEventListener("visibilitychange", this.handlerVisibilityChange) // evento que nos informa si la pagina es visible, pude ser usado para ejecutar una acción cuando cambiamos de pestaña. Así podemos ahorrar batería y mejorar la UX.
     }
 
     //recibimos todos los objetos que estamos observando
@@ -17,6 +20,15 @@ class AutoPause {
         const entry = entries[0]
 
         const isVisible = entry.intersectionRatio >= this.threshold
+        if (isVisible) {
+            this.player.play()
+        } else {
+            this.player.pause()
+        }
+    }
+
+    handlerVisibilityChange() {
+        const isVisible = document.visibilityState === "visible"
         if (isVisible) {
             this.player.play()
         } else {
